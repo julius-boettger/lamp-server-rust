3714c18c29c406c6b95d5abd1ee2efe68e2d5593
julius-boettger <julius.btg@proton.me>
2023-11-15 17:32:44 +0100

    feat: implement send_api_request()

--- src/util/govee.rs
+++ src/util/govee.rs
@@ -1,14 +1,24 @@
+use crate::util;
+use crate::res::secrets::govee;
+
 type RGBColor = (u8, u8, u8);
 
-enum SetStatus {
+enum SetState {
     Color(RGBColor),
     Brightness(u8),
     Power(bool)
 }
 
-fn set_status(status: SetStatus) {
+fn set_state(status: SetState) {
     if cfg!(govee_debug) {
-        // handle debug mode
+        // TODO handle debug mode
         return;
     }
+}
+
+async fn get_power_state() {
+    let url = format!("https://developer-api.govee.com/v1/devices/state?device={}&model={}", govee::DEVICE, govee::MODEL);
+    let response = util::send_api_request(url.as_str(), util::HttpMethod::Get).await;
+    println!("{:?}", response.unwrap().text().await);
+    // TODO error handling, parse json response, return result
 }
--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,5 +1,25 @@
 pub mod govee;
+pub enum HttpMethod { Get, Put }
 
 pub fn digest_sha256(string: &str) -> String {
     sha256::digest(string)
+}
+
+pub async fn send_api_request(url: &str, method: HttpMethod) -> Option<reqwest::Response> {
+    let client =  reqwest::Client::new();
+    let mut request = match method {
+        HttpMethod::Get => client.get(url),
+        HttpMethod::Put => client.put(url)
+    };
+
+    // TODO headers as function argument
+    request = request.header("Govee-API-Key", crate::res::secrets::govee::API_KEY);
+    //request = request.header("Content-Type", "application/json");
+
+    if let HttpMethod::Put = method {
+        // TODO append body to request
+    }
+    
+    // send request and try to return Some(Response)
+    request.send().await.ok()
 }
