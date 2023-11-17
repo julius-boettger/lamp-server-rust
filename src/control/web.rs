1c3895b787726d0868a2963719d541e73d4601fb
julius-boettger <julius.btg@proton.me>
2023-11-17 21:55:00 +0100

    refactor: extract to control::web

---
+++ src/control/web.rs
@@ -0,0 +1,49 @@
+#[utoipa::path(
+    get,
+    path = "/test",
+    responses((
+        status = 200,
+        description = "Send an http GET-request and try to parse the response to json. Return success as string."
+    ))
+)]
+async fn test() -> &'static str {
+    let response = reqwest::get("https://httpbin.org/ip").await;
+    if response.is_err() { return "error sending request"; }
+    let json = response.unwrap().json::<std::collections::HashMap<String, String>>().await;
+    if json.is_err() { return "error parsing json"; }
+    "got json response"
+}
+
+/// start webserver. never terminates
+async fn start_server() {
+    use crate::res::constants::*;
+    use axum::routing::get;
+    use axum::response::Redirect;
+    use utoipa::OpenApi;
+    use utoipa_swagger_ui::SwaggerUi;
+
+    #[derive(OpenApi)]
+    #[openapi(
+        paths(
+            test,
+        ),
+        tags((name = "lamp-server-rust", description = "API for interacting with my lamp"))
+    )]
+    struct ApiDoc;
+
+    let app = axum::Router::new()
+        // swagger ui
+        .merge(SwaggerUi::new("/swagger-ui")
+            .url("/openapi.json", ApiDoc::openapi()))
+        // temporarily redirect root to swagger ui
+        .route("/", get(|| async { Redirect::temporary("/swagger-ui") }))
+        // actual api
+        .route("/test", get(test));
+
+    let address = std::net::SocketAddr::new(LOCALHOST, PORT);
+    println!("starting server on http://{address} ...");
+    axum::Server::bind(&address)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -8,58 +8,9 @@ debug mode:
 mod res;
 mod util;
 mod view;
-
-use res::constants::*;
-
-#[utoipa::path(
-    get,
-    path = "/test",
-    responses((
-        status = 200,
-        description = "Send an http GET-request and try to parse the response to json. Return success as string."
-    ))
-)]
-async fn test() -> &'static str {
-    let response = reqwest::get("https://httpbin.org/ip").await;
-    if response.is_err() { return "error sending request"; }
-    let json = response.unwrap().json::<std::collections::HashMap<String, String>>().await;
-    if json.is_err() { return "error parsing json"; }
-    "got json response"
-}
-
-async fn start_webserver() {
-    use axum::routing::get;
-    use axum::response::Redirect;
-    use utoipa::OpenApi;
-    use utoipa_swagger_ui::SwaggerUi;
-
-    #[derive(OpenApi)]
-    #[openapi(
-        paths(
-            test,
-        ),
-        tags((name = "lamp-server-rust", description = "API for interacting with my lamp"))
-    )]
-    struct ApiDoc;
-
-    let app = axum::Router::new()
-        // swagger ui
-        .merge(SwaggerUi::new("/swagger-ui")
-            .url("/openapi.json", ApiDoc::openapi()))
-        // temporarily redirect root to swagger ui
-        .route("/", get(|| async { Redirect::temporary("/swagger-ui") }))
-        // actual api
-        .route("/test", get(test));
-
-    let address = std::net::SocketAddr::new(LOCALHOST, PORT);
-    println!("starting server on http://{address} ...");
-    axum::Server::bind(&address)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
-}
+mod control;
 
 #[tokio::main]
 async fn main() {
-    start_webserver().await;
+    control::web::start_server().await;
 }
