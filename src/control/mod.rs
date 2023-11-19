0a59ebea4a6aeb429f371f7d0c615d690c259728
julius-boettger <julius.btg@proton.me>
2023-11-19 21:54:16 +0100

    feat: implement generate_sunrise()

--- src/control/mod.rs
+++ src/control/mod.rs
@@ -2,19 +2,69 @@ pub mod web;
 
 use crate::util::govee;
 use crate::res::constants;
+use govee::SetState;
+use std::time::Duration;
 use std::collections::VecDeque;
 
 /// never terminates
 pub async fn main_loop() {
+    use crate::res::constants::govee::API_REQUEST_INTERVAL;
+    use std::thread::sleep;
+
+    {
+        use constants::sunrise::*;
+        if govee_brightness::START >= govee_brightness::STOP {
+            panic!("sunrise brightness has to start smaller than it stops");
+        }
+        if hsv_color::saturation::START <= hsv_color::saturation::STOP {
+            panic!("sunrise color saturation has to start larger than it stops");
+        }
+    };
 
     // queue of `SetState`s of which the first one will be executed each iteration
-    let mut govee_queue: VecDeque<govee::SetState> = VecDeque::new();
+    let mut govee_queue: VecDeque<SetState> = VecDeque::new();
+
+    generate_sunrise(&mut govee_queue, Duration::from_secs(1 * 60));
 
     loop {
         if !govee_queue.is_empty() {
             govee::set_state(govee_queue.pop_front().unwrap()).await;
         }
         println!("----- waiting -----");
-        std::thread::sleep(constants::govee::API_REQUEST_INTERVAL);
+        sleep(API_REQUEST_INTERVAL);
+    }
+}
+
+/// append `SetState`s for a sunrise of given duration to `govee_queue`
+fn generate_sunrise(govee_queue: &mut VecDeque<SetState>, sunrise_duration: Duration) {
+    use crate::res::constants::govee::*;
+    use crate::res::constants::sunrise::*;
+
+    // number of `SetState`s to generate for brightness and color each.
+    // f64 type is needed for later calculations.
+    let state_amount = (
+        sunrise_duration.as_millis() /
+        (API_REQUEST_INTERVAL.as_millis() + AVG_SET_STATE_DURATION.as_millis()) /
+        2 // for brightness and color each
+    ) as f64;
+
+    let brightness_step = (govee_brightness::STOP - govee_brightness::START) / (state_amount - 1.0);
+    let saturation_step = (hsv_color::saturation::START - hsv_color::saturation::STOP) / (state_amount - 1.0);
+
+    // `while` because `for` is not possible with f64
+    let mut iteration = 0.0f64;
+    while iteration < state_amount {
+        govee_queue.push_back(SetState::Brightness(
+            (govee_brightness::START + (brightness_step * iteration))
+                .round() as u8
+        ));
+        govee_queue.push_back(SetState::Color(
+            hsv::hsv_to_rgb(
+                hsv_color::HUE,
+                hsv_color::saturation::START - (saturation_step * iteration),
+                hsv_color::VALUE,
+            )
+        ));
+        iteration += 1.0;
     }
 }
--- src/res/constants.rs
+++ src/res/constants.rs
@@ -4,6 +4,24 @@ pub mod govee {
     use std::time::Duration;
     // TODO recalculate
     pub const API_REQUEST_INTERVAL: Duration = Duration::from_millis(8730);
+    /// how long a `set_state()` call usually takes
+    pub const AVG_SET_STATE_DURATION: Duration = Duration::from_millis(500);
+}
+
+// f64 types for easier calculations
+pub mod sunrise {
+    pub mod hsv_color {
+        pub const HUE: f64 = 25.0;
+        pub mod saturation {
+            pub const START: f64 = 0.8;
+            pub const STOP: f64 = 0.45;
+        }
+        pub const VALUE: f64 = 1.0;
+    }
+    pub mod govee_brightness {
+        pub const START: f64 = 1.0;
+        pub const STOP: f64 = 100.0;
+    }
 }
 
 pub mod net {
