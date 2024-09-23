307e79ab815e7f74d32618d62dab880582043500
julius-boettger <julius.btg@proton.me>
2024-09-23 11:36:00 +0200

    nix: simplify package for new secret handling

--- flake.nix
+++ flake.nix
@@ -27,37 +27,23 @@
     packages = eachSystem (system:
     let
       pkgs = import inputs.nixpkgs { inherit system; };
-      lamp-server-pkg =
-        {
-          # govee secrets that should be overridden
-          govee_api_key ? "",
-          govee_device  ? "",
-          govee_model   ? ""
-        }:
-        pkgs.rustPlatform.buildRustPackage {
-          name = "lamp-server";
-          src = ./.;
-          cargoLock.lockFile = ./Cargo.lock;
-
-          nativeBuildInputs = [ pkgs.pkg-config ];
-          configurePhase = ''
-            runHook preConfigure
-
-            # fix https://github.com/sfackler/rust-openssl/issues/1663
-            export PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig"
-
-            cat <<EOF > src/constants/govee_secrets.rs
-              pub const API_KEY: &str = "${govee_api_key}";
-              pub const DEVICE:  &str = "${govee_device}";
-              pub const MODEL:   &str = "${govee_model}";
-            EOF
-
-            runHook postConfigure
-          '';
-        };
     in
     {
-      lamp-server = pkgs.callPackage lamp-server-pkg {};
+      lamp-server = pkgs.rustPlatform.buildRustPackage {
+        name = "lamp-server";
+        src = ./.;
+        cargoLock.lockFile = ./Cargo.lock;
+
+        nativeBuildInputs = [ pkgs.pkg-config ];
+        configurePhase = ''
+          runHook preConfigure
+
+          # fix https://github.com/sfackler/rust-openssl/issues/1663
+          export PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig"
+
+          runHook postConfigure
+        '';
+      };
     });
   };
 }
